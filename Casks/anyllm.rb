c9077d158508bfd13d54020e6c3125cda254f34e
jeryfan <63136443+jeryfan@users.noreply.github.com>
2026-02-14 14:24:54 +0000

    chore: bump to v0.2.0

--- Casks/anyllm.rb
+++ Casks/anyllm.rb
@@ -1,18 +1,18 @@
 cask "anyllm" do
-    version "0.1.0"
-    sha256 arm:   "3fa92f59a081b9f86ab47a6de0ebb36016e0dd00f1e07062ada0d86862e51aff",
-           intel: "b721fb00b140f752755b16a1ba6c5bb34dddbf1344f498f020945759d9ffd4dc"
+  version "0.2.0"
+  sha256 arm:   "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5",
+         intel: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
-    on_arm do
-      url "https://github.com/jeryfan/anyllm/releases/download/v#{version}/AnyLLM_#{version}_aarch64.dmg"
-    end
-    on_intel do
-      url "https://github.com/jeryfan/anyllm/releases/download/v#{version}/AnyLLM_#{version}_x64.dmg"
-    end
-
-    name "AnyLLM"
-    desc "LLM API gateway desktop app"
-    homepage "https://github.com/jeryfan/anyllm"
-
-    app "AnyLLM.app"
+  on_arm do
+    url "https://github.com/jeryfan/anyllm/releases/download/v#{version}/AnyLLM_#{version}_aarch64.dmg"
   end
+  on_intel do
+    url "https://github.com/jeryfan/anyllm/releases/download/v#{version}/AnyLLM_#{version}_x64.dmg"
+  end
+
+  name "AnyLLM"
+  desc "LLM API gateway desktop app"
+  homepage "https://github.com/jeryfan/anyllm"
+
+  app "AnyLLM.app"
+end
